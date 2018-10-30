9558a3d57eb6d73c8aa98b4191906f29a4af910c
Ryan Daley <rdaley321@yahoo.com>
2018-10-30 02:25:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   return nil if array.nil?
   return array[0] if array.length == 1
-  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+  return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
 end
